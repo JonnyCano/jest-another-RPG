1804d17e76e2a983f3b98c18cc7898b86c26e475
Jonathan Cano <jcano528@gmail.com>
2021-09-03 02:00:53 -0500

    created and tested the Enemy constructor object for the first time

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,54 @@
+const Enemy = require('../lib/Enemy');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
+
+test("gets enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    // the expect.stringContaining() method is an expect method that adds flexibility to change in design
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+test("gets enemy's attack value", () => {
+    const enemy = new Enemy('Jonny');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("subtracts from enemy's health", () => {
+    const enemy = new Enemy('Jonny');
+    const OldHealth = enemy.health;
+
+    enemy.reduceHealth(5);
+
+    expect(enemy.health).toBe(OldHealth - 5);
+
+    enemy.reduceHealth(99999);
+
+    expect(enemy.health).toBe(0);
+});
+
