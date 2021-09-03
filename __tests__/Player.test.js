2770e79de1d6fb6a57cd17df14cfac841385dacd
Jonathan Cano <jcano528@gmail.com>
2021-09-02 23:50:59 -0500

    implemented the TDD on new Player object methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,9 +4,9 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion.js');
 
 test('creates a Player object', () => {
-    const player = new Player('Dave');
+    const player = new Player('Jonny');
 
-    expect(player.name).toBe('Dave');
+    expect(player.name).toBe('Jonny');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
@@ -18,7 +18,7 @@ test('creates a Player object', () => {
 });
 
 test("gets player's stats as an object", () => {
-    const player = new Player('Dave');
+    const player = new Player('Jonny');
 
     // player.getStats should return an object with four specific properties
     expect(player.getStats()).toHaveProperty('potions');
@@ -28,7 +28,7 @@ test("gets player's stats as an object", () => {
 });
 
 test('gets inventory from player or returns false', () => {
-    const player = new Player('Dave');
+    const player = new Player('Jonny');
 
     expect(player.getInventory()).toEqual(expect.any(Array));
 
@@ -37,3 +37,32 @@ test('gets inventory from player or returns false', () => {
     expect(player.getInventory()).toEqual(false);
 });
 
+test("gets player's health value", () => {
+    const player = new Player('Jonny');
+
+    // the expect.stringContaining() method is an expect method that adds flexibility to change in design
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Jonny');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Jonny');
+    const OldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(OldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
